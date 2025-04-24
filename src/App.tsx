fe1764538637985b9bc2cc8d239158d6cd1db08d
Henrik Baugerød <henrikbaugerod@hotmail.com>
2025-04-24 08:41:15 +0200

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR TESTING  2:)
+          Edit <code>src/App.tsx</code> bla bla bla:)
         </p>
       </div>
       <p className="read-the-docs">
